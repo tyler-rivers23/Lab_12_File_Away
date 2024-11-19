567155d8cbf7c9ca1afc9f2059acf14c8579622a
videogamelife8 <tyler.rivers2023@gmail.com>
2024-11-19 15:15:53 -0500

    Initial commit

---
+++ src/FileInspector.java
@@ -0,0 +1,109 @@
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+/**
+ *
+ * @author wulft
+ * 
+ * Use the thread safe NIO IO library to read a file
+ */
+public class FileInspector
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec = "";
+        int line = 0;
+        int wordCnt = 0;
+        int charCnt = 0;
+        String[] words;
+        ArrayList<String> lines = new ArrayList<>();
+        
+       try
+       {
+        // uses a fixed known path:
+       //  Path file = Paths.get("c:\\My Documents\\data.txt");
+        
+       // use the toolkit to get the current working directory of the IDE
+       // Not sure if the toolkit is thread safe...
+       File workingDirectory = new File(System.getProperty("user.dir"));       
+       
+       // Typiacally, we want the user to pick the file so we use a file chooser
+       // kind of ugly code to make the chooser work with NIO.
+       // Because the chooser is part of Swing it should be thread safe.
+       chooser.setCurrentDirectory(workingDirectory);
+       // Using the chooser adds some complexity to the code.
+       // we have to code the complete program within the conditional return of
+       // the filechooser because the user can close it without picking a file
+       
+       if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+       {
+          selectedFile = chooser.getSelectedFile(); 
+          Path file = selectedFile.toPath();
+           // Typical java pattern of inherited classes
+           // we wrap a BufferedWriter around a lower level BufferedOutputStream
+           InputStream in =
+                   new BufferedInputStream(Files.newInputStream(file, CREATE));
+           BufferedReader reader = 
+                   new BufferedReader(new InputStreamReader(in));
+           
+           // Finally we can read the file LOL!
+
+           while(reader.ready())
+           {
+              rec = reader.readLine();
+              lines.add(rec);
+              line++;
+              charCnt += rec.length();
+              words = rec.split(" ");
+              wordCnt += words.length;
+              // echo to screen
+               System.out.printf("\nLine %4d %-60s ", line, rec);
+           }
+           for(String l:lines)
+           {
+               System.out.println(l);
+           }
+           
+           String fields[] = lines.get(5).split(", ");
+           for(String f:fields)
+             System.out.println(f);
+           
+           reader.close(); // must close the file to seal it and flush buffer
+           System.out.println("\n\nData file read!");
+           System.out.println("File" + selectedFile.getName());
+           System.out.println("File Stats: ");
+           System.out.println("Total lines: " + line);
+           System.out.println("Total words: " + wordCnt);
+           System.out.println("Total characters: " + charCnt);
+
+       }
+       else  // user closed the file dialog wihtout choosing
+       {
+           System.out.println("Failed to choose a file to process");
+           System.out.println("Run the program again!");
+           System.exit(0);
+       }
+       }  // end of TRY
+       catch (FileNotFoundException e)
+       {
+           System.out.println("File not found!!!");
+           e.printStackTrace();
+       }
+       catch (IOException e)
+       {
+           e.printStackTrace();
+       }
+    }
+    
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+
+
+
+
+}
+
